3f7f4325eb6640bfb783d2929c67f903079f45d8
tochman <thomas@craftacademy.se>
2018-04-28 11:00:40 +0200

    generilizes product path

--- spec/dummy/config/initializers/cartify.rb
+++ spec/dummy/config/initializers/cartify.rb
@@ -7,6 +7,8 @@ Cartify.price_attribute = :price
 # user_class:      is a owner model name, change it if you have other!
 # product_class:   is a product model name, change it if you have other!
 # empty_cart_path: it's a route where you will be redirected if empty cart and user try go to checkout
+# title_attribute: is the attribute used to display the name or title of your product
+# price_attribute: is the attribute that holds price of your product
 
 # don't forget add:
 # mount Cartify::Engine, at: '/'  # into routes.rb file
